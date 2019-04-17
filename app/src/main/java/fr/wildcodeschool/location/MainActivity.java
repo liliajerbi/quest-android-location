8fd568585e11cd17a6765bbb967efe04b8034f59
Lilia Jerbi <lilia-jerbi@hotmail.fr>
2019-04-17 17:40:03 +0200

    quete

--- app/src/main/java/fr/wildcodeschool/location/MainActivity.java
+++ app/src/main/java/fr/wildcodeschool/location/MainActivity.java
@@ -3,11 +3,18 @@ package fr.wildcodeschool.location;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +33,44 @@ public class MainActivity extends AppCompatActivity {
         final TextView tvLocation = findViewById(R.id.tvLocation);
 
         // TODO : your code here
+        LocationManager locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+        FusedLocationProviderClient fusedLocationClient =  LocationServices.getFusedLocationProviderClient(this);
+        fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                // Got last known location. In some rare situations this can be null.
+                tvLocation.append("Update location : " + "\n" + location.toString() + "\n");
+                if (location != null) {
+                    // Logic to handle location object
+                    tvLocation.append("Last location : " + "\n" + location.toString());
+                }
+            }
+        });
     }
 
     private void checkLocationPermission() {
